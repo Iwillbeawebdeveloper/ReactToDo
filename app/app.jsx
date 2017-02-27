4b76af6163b6e222ce649431e0c642e9e949b2cf
James Winfield <jamesthemonkeh@hotmail.com>
2017-02-27 09:20:57 +0000

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+// Load foundation
+$(document).foundation();
+
+//App css
+require('style!css!sass!ApplicationStyles');
+
+ReactDOM.render(
+    <p>Boilerplate Project</p>
+    ,
+    document.getElementById('app')
+);
