2e214f4441cfe5fee9c95d42626ea89712d11195
James Winfield <jamesthemonkeh@hotmail.com>
2017-03-05 11:49:47 +0000

    Modifying components

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -35,7 +35,7 @@ describe('Reducers', () => {
         
             expect(res.length).toEqual(1);
             expect(res[0].text).toEqual(action.text);
-        })
+        });
         it('should toggle todo', () => {
             var todos = [{
                 id: '123',
@@ -52,6 +52,23 @@ describe('Reducers', () => {
             
             expect(res[0].completed).toEqual(false);
             expect(res[0].completedAt).toEqual(undefined);
-        })
-    })
+        });
+        it ('should add existing todos', () => {
+            var todos = [{
+            id: '111',
+            test: 'antyhing',
+            completed: false,
+            completedAT: undefined,
+            createdAt: 33000
+            }];
+            var action = {
+                type: 'ADD_TODOS',
+                todos
+            };
+            var res = reducers.todosReducer(dj([]), df(action));
+            
+            expect(res.length).toEqual(1);
+            expect(res[0]).toEqual(todos[0]);
+        });
+    });
 });
