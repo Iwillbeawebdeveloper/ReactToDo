5e159ba9f90cb5a31344beffff0d5fdef99f6283
James Winfield <jamesthemonkeh@hotmail.com>
2017-03-07 09:44:17 +0000

    Add actions for firebase login and logout

---
+++ app/components/Todos.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+export var Todos = react.CreateClass({
+    render: function() {
+        return (
+            <p>This is the log in page</p>
+        )
+    }
+});
+
+module.exports = Todos;
